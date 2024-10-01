fa6969604ee32e8f3e70af9b32667cc5f325e14f
vovakalinins <vovakalinin24@icloud.com>
2024-10-01 14:38:09 -0400

    http server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
+use axum::extract::State;
+use axum::{routing::post, Json, Router};
 use ollama_rs::generation::completion::request::GenerationRequest;
 use ollama_rs::Ollama;
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::sync::Arc;
 
 #[tokio::main]
+
 async fn main() {
-    println!("Starting server on http://localhost:11434");
-    let ollama = Ollama::default();
+    println!("Starting Ollama Rust Server...");
 
-    println!("Server should be running");
+    let ollama = Arc::new(Ollama::default()); // Arc thread, tread safe shared state
 
-    prompt("Hello How Are You?".to_string(), ollama).await;
+    let app = Router::new().route("/prompt", post(handle_prompt).with_state(ollama.clone()));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 14434));
+    println!("Listening for Prompts on http://{}/prompt", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-async fn prompt(prompt: String, ollama: Ollama) {
+#[derive(Deserialize)]
+struct PromptRequest {
+    prompt: String,
+}
+
+#[derive(Serialize)]
+struct PromptResponse {
+    response: String,
+}
+
+async fn handle_prompt(
+    State(ollama): State<Arc<Ollama>>,
+    Json(payload): Json<PromptRequest>,
+) -> Json<PromptResponse> {
+    let response = prompt(payload.prompt, ollama.clone()).await;
+
+    Json(PromptResponse { response })
+}
+
+async fn prompt(prompt: String, ollama: Arc<Ollama>) -> String {
     let model = "llama3.1:latest".to_string();
 
     let res = ollama.generate(GenerationRequest::new(model, prompt)).await;
 
-    if let Ok(res) = res {
-        println!("{}", res.response);
-    } else {
-        eprintln!("Failed to generate response.");
+    match res {
+        Ok(res) => res.response,
+        Err(_) => "Failed to generate response.".to_string(),
     }
 }
